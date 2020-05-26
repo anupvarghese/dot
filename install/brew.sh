9014d01d69d1773fcb9874e814b1281f654ba958
Anoop Varghese <avarghese@atlassian.com>
2020-05-26 11:49:15 +1000

    add brew taps

--- install/brew.sh
+++ install/brew.sh
@@ -3,7 +3,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew update
 brew upgrade
 
-brew install wifi-password dockutil n docker
+brew install wifi-password dockutil n docker terraform awscli azure-cli
 
 brew cask install --appdir="/Applications" iterm2
 brew cask install --appdir="/Applications" visual-studio-code
