ede8bcfde9243ee5b5aa0b48477cec570eaefab2
Anoop Varghese <anupvarghese@users.noreply.github.com>
2021-10-05 07:57:22 +1100

    Linux (#9)
    * modify for linux
    
    * update nvim tree
    
    * add script for fonts
    
    * remove defaults

--- install/install.sh
+++ install/install.sh
@@ -1,23 +1,26 @@
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
-echo "🍻 Homebrew setup"
-. "$DOTFILES_DIR/install/brew.sh"
+if [[ "$OSTYPE" == "darwin" ]]; then
+	echo "🍻 Homebrew setup"
+	. "$DOTFILES_DIR/install/brew.sh"
+
+  echo "💻 Mac specific configs"
+	. "$DOTFILES_DIR/mac/defaults.sh"
+
+	echo "⚓ Configure dock"
+	. "$DOTFILES_DIR/mac/dock.sh"
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+	echo "Linux setup"
+	. "$DOTFILES_DIR/install/linux.sh"
+fi
+
 
 echo "🕹️ Console configuration"
 . "$DOTFILES_DIR/install/zsh.sh"
 
 echo "🔗 Symlinks"
 . "$DOTFILES_DIR/install/symlinks.sh"
-
-echo "💻 Mac specific configs"
-. "$DOTFILES_DIR/mac/defaults.sh"
-
-echo "⚓ Configure dock" 
-. "$DOTFILES_DIR/mac/dock.sh"
-
-# echo "VSCode settings"
-#. "$DOTFILES_DIR/vscode/extensions.sh"
 
 echo "Clear all"
 . "$DOTFILES_DIR/bin/dotfiles" clean
