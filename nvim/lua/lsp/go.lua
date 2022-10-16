2c74a10abb7b59dd6f70cf8390126f3113854eb1
Anoop Varghese <anupvarghese@gmail.com>
2022-10-16 12:53:39 +1100

    add lsp zero

--- nvim/lua/lsp/go.lua
+++
@@ -1,8 +0,0 @@
-local lsp_config = require("lspconfig")
-local on_attach, lsp_flags, capabilities = require("lsp/on_attach")()
-
-lsp_config.gopls.setup({
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities
-})
